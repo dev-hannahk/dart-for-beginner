407014f4d2f2adfffb18cf074d46dfe81f4530d2
dev-hannahk <dev.hannahk@gmail.com>
2023-02-05 00:22:36 +0900

    Late Variables

--- main.dart
+++ main.dart
@@ -1,7 +1,8 @@
 void main() {
-  final name = 'hannah';
-  name = '해나';
-  // throw error
-  // The final variable 'name' can only be set once.
-  // Try making 'name' non-final.
+  // 초기 데이터 없이 변수 선언이 가능
+  late final String name;
+  // print(name); // 할당 전에 접근 불가능
+  name = 'hannah';
+  // name = '해나' // final 변수이기 때문에 재할당 불가능
+  print(name);
 }
