f63e1470af457817e426c816425e8df728f402c5
dev-hannahk <dev.hannahk@gmail.com>
2023-02-05 00:27:26 +0900

    Constant Variables

--- main.dart
+++ main.dart
@@ -1,8 +1,4 @@
 void main() {
-  // 초기 데이터 없이 변수 선언이 가능
-  late final String name;
-  // print(name); // 할당 전에 접근 불가능
-  name = 'hannah';
-  // name = '해나' // final 변수이기 때문에 재할당 불가능
-  print(name);
+  // const는 컴파일 타임에 알고 있는 값이어야 한다
+  const API = 'http://localhost:8080/';
 }
