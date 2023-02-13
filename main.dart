ab1eb516448fd23be5f2700a671cb73714108b1f
dev-hannahk <dev.hannahk@gmail.com>
2023-02-13 22:15:05 +0900

    Lists

--- main.dart
+++ main.dart
@@ -1,11 +1,6 @@
 void main() {
-  String name = 'hananh';
-  bool alive = true;
-  int age = 20;
-  // abstract class int extends num
-  double money = 12.11;
-  // abstract class double extends num
-  // num으로 부터 파생된 타입
-  num x = 12;
-  x = 1.1;
+  var giveMeFive = true;
+  // List<int> numbers
+  var numbers = [1, 2, 3, 4, if (giveMeFive) 5];
+  print(numbers);
 }
