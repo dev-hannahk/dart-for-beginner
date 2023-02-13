3d2c3154b1a6ca906714ce29e728516448b9c9ea
dev-hannahk <dev.hannahk@gmail.com>
2023-02-13 22:39:56 +0900

    Sets

--- main.dart
+++ main.dart
@@ -1,18 +1,9 @@
 void main() {
-  // Map<String, Object> player
-  // Object like any in TS
-  var player = {'name': 'hannah', 'xp': 19.99, 'power': false};
-
-  Map<int, bool> another = {1: true, 2: false, 0: false};
-
-  Map<List<int>, bool> theOther = {
-    [0, 1]: true,
-    [2, 3]: false,
-    [0, 3]: false
-  };
-
-  List<Map<String, Object>> players = [
-    {'name': 'hannah'},
-    {'name': 'haley'},
-  ];
+  var numbers = {1, 2, 3, 4};
+  Set<int> anoutherNumbers = {1, 2, 3, 4};
+  // all the item is unique values
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers); // {1, 2, 3, 4}
 }
