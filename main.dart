c2139c0a03ed98b5dab06f723c9d5032629a1bb8
dev-hannahk <dev.hannahk@gmail.com>
2023-02-19 01:27:29 +0900

    Class - Named constructor parameters

--- main.dart
+++ main.dart
@@ -1,25 +1,25 @@
 class Player {
-  // late String name;
-  // late int xp;
-
   final String name;
   int xp;
+  String team;
+  int age;
 
-  // Player(String name, int xp) {
-  //   this.name = name;
-  //   this.xp = xp;
-  // }
-
-  Player(this.name, this.xp);
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
 
   void sayHello() {
-    print("Hi, I'm $name.");
+    print("Hi, I'm $name. My team is $team and my xp is $xp.");
   }
 }
 
 void main() {
-  var player1 = Player('hannah', 10000);
+  var player1 = Player(name: 'hannah', xp: 1000, team: 'blue', age: 20);
+  // player1.name = "Fake"; // 'name' can't be used as a setter because it's final.
   player1.sayHello();
-  var player2 = Player('haley', 20000);
+
+  var player2 = Player(name: 'haley', xp: 2000, team: 'red', age: 22);
   player2.sayHello();
 }
