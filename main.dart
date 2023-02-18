2d84773e03b56ff0fa4f50f130cfbd5fb5b0d513
dev-hannahk <dev.hannahk@gmail.com>
2023-02-19 00:15:35 +0900

    Defining a Function

--- main.dart
+++ main.dart
@@ -1,9 +1,7 @@
+String sayHello(String name) => "Hello $name";
+
+num plus(num a, num b) => a + b;
+
 void main() {
-  var numbers = {1, 2, 3, 4};
-  Set<int> anoutherNumbers = {1, 2, 3, 4};
-  // all the item is unique values
-  numbers.add(1);
-  numbers.add(1);
-  numbers.add(1);
-  print(numbers); // {1, 2, 3, 4}
+  sayHello('hannah');
 }
