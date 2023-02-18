2c11c679d05f8b9a900dc8894e213c89b6d4cddf
dev-hannahk <dev.hannahk@gmail.com>
2023-02-19 00:50:56 +0900

    Typedef - part 1

--- main.dart
+++ main.dart
@@ -1,25 +1,16 @@
-// String capitalizeName(String? name) {
-//   if (name != null) {
-//     return name.toUpperCase();
-//   }
-//   return 'ANONYMOUS';
+// typedef allows to create alias for the types.
+typedef ListOfInts = List<int>;
+
+// List<int> reverseListOfNumbers(List<int> list) {
+//   var reversed = list.reversed;
+//   return reversed.toList();
 // }
 
-// String capitalizeName(String? name) =>
-//     name != null ? name.toUpperCase() : 'ANONYMOUS';
-
-// if left is null return right.
-// if left is not null return left.
-String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANONYMOUS';
+ListOfInts reverseListOfNumbers(List<int> list) {
+  var reversed = list.reversed;
+  return reversed.toList();
+}
 
 void main() {
-  capitalizeName('hannah');
-  capitalizeName(null);
-  String? name;
-  // if name is null, put a value inside of it.
-  name ??= 'hannah';
-  print(name);
-  name = null;
-  name ??= 'haley';
-  print(name);
+  print(reverseListOfNumbers([1, 2, 3])); // [3, 2, 1]
 }
