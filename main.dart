61854b899930462f6e6a1c0c4cbd332f35b8d168
dev-hannahk <dev.hannahk@gmail.com>
2023-02-19 00:43:56 +0900

    QQ Operator

--- main.dart
+++ main.dart
@@ -1,9 +1,25 @@
-// optional positional parameter
-String sayHello(String name, int age, [String? country = 'korea']) {
-  return "Hello $name, you are $age, and you come from $country.";
-}
+// String capitalizeName(String? name) {
+//   if (name != null) {
+//     return name.toUpperCase();
+//   }
+//   return 'ANONYMOUS';
+// }
+
+// String capitalizeName(String? name) =>
+//     name != null ? name.toUpperCase() : 'ANONYMOUS';
+
+// if left is null return right.
+// if left is not null return left.
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANONYMOUS';
 
 void main() {
-  var results = sayHello('hannah', 20);
-  print(results);
+  capitalizeName('hannah');
+  capitalizeName(null);
+  String? name;
+  // if name is null, put a value inside of it.
+  name ??= 'hannah';
+  print(name);
+  name = null;
+  name ??= 'haley';
+  print(name);
 }
