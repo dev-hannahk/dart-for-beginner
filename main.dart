f3990c27be8f2038c5553c7402e48a481d420685
dev-hannahk <dev.hannahk@gmail.com>
2023-02-19 00:34:26 +0900

    Optional Positional Parameter

--- main.dart
+++ main.dart
@@ -1,20 +1,9 @@
-// 1. positional parameter
-// String sayHello(String name, int age, String country) {
-//   return "Hello $name, you are $age, and you come from $country";
-// }
-
-// 2. named parameter
-// set default value
-// String sayHello({String name = '', int age = 0, String country = ''}) {
-//   return "Hello $name, you are $age, and you come from $country";
-// }
-
-String sayHello(
-    {required String name, required int age, required String country}) {
-  return "Hello $name, you are $age, and you come from $country";
+// optional positional parameter
+String sayHello(String name, int age, [String? country = 'korea']) {
+  return "Hello $name, you are $age, and you come from $country.";
 }
 
 void main() {
-  // sayHello('hannah', 20, 'korea');
-  sayHello(age: 12, country: 'korea', name: 'hannah');
+  var results = sayHello('hannah', 20);
+  print(results);
 }
