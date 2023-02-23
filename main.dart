032284e3a7d2d08ee96142000b0fa9046d3c6622
dev-hannahk <dev.hannahk@gmail.com>
2023-02-23 21:06:03 +0900

    Abstract Classes

--- main.dart
+++ main.dart
@@ -1,8 +1,20 @@
+// Abstract Classes
+// 추상화 클래스는 다른 클래스들이 직접 구현해야하는 메소드들을 모아놓은 일종의 청사진이다
+abstract class Human {
+  void walk();
+}
+
 enum Team { blue, red }
 
 enum XPLevel { beginner, pro, intermediate }
 
-class Player {
+class Coach extends Human {
+  void walk() {
+    print("The coach is walking");
+  }
+}
+
+class Player extends Human {
   String name;
   XPLevel xp;
   Team team;
@@ -11,6 +23,10 @@ class Player {
 
   void sayHello() {
     print("Hi, I'm $name. My team is $team and my xp is $xp.");
+  }
+
+  void walk() {
+    print('$name is walking.');
   }
 }
 
